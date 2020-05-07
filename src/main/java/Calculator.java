60a8f637ab39f7d40bce62e8f6e8e4f4df2020f5
dhruvin32 <dhruvinradadiya25@gmail.com>
2020-05-07 17:07:57 +0530

    commit for command line

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,14 +6,14 @@ public class Calculator {
     }
 
     public static void main(String[] args){
-        //double a = Double.parseDouble(args[0]);
-        //double b = Double.parseDouble(args[1]);
-        Scanner sc= new Scanner(System.in);
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        //Scanner sc= new Scanner(System.in);
         //double a = Double.parseDouble("1");
         //double b = Double.parseDouble("2");
 
-        double a = Double.parseDouble(sc.nextLine());
-        double b = Double.parseDouble(sc.nextLine());
+        //double a = Double.parseDouble(sc.nextLine());
+        //double b = Double.parseDouble(sc.nextLine());
 
         Calculator calculator = new Calculator();
         System.out.println("Sum of two number " + a + " and " + b +" is " + calculator.evaluate(a,b));
